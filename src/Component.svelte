dbfbc33e33f4a8f012a63b18473fad06ea8e301f
poirazis <poirazis@gmail.com>
2024-06-01 21:41:40 +0300

    Moved to common css for all field components

--- src/Component.svelte
+++ src/Component.svelte
@@ -1,8 +1,17 @@
 <script>
-  import { getContext , onDestroy} from "svelte";
+  import { getContext, onDestroy } from "svelte";
   import CellLink from "../../bb_super_components_shared/src/lib/SuperTableCells/CellLink.svelte";
+  import "../../bb_super_components_shared/src/lib/SuperFieldsCommon.css";
 
-  const { styleable, Block, BlockComponent, Provider, builderStore, componentStore, API } = getContext("sdk");
+  const {
+    styleable,
+    Block,
+    BlockComponent,
+    Provider,
+    builderStore,
+    componentStore,
+    API,
+  } = getContext("sdk");
   const component = getContext("component");
 
   const formContext = getContext("form");
@@ -11,49 +20,49 @@
   const labelWidth = getContext("field-group-label-width");
   const formApi = formContext?.formApi;
 
-  export let linkValueType = "link"
+  export let linkValueType = "link";
   export let field;
-  export let filter
-  export let validation
+  export let filter;
+  export let validation;
 
-  export let controlType = "select"
-  export let customButtons
+  export let controlType = "select";
+  export let customButtons;
   export let buttons = [];
   export let buttonsQuiet;
 
   export let label;
   export let span = 6;
-  export let placeholder
-  export let defaultValue
-  export let disabled
-  export let readonly
-  export let autocomplete
+  export let placeholder;
+  export let defaultValue;
+  export let disabled;
+  export let readonly;
+  export let autocomplete;
 
-  export let tableId
-  export let filterCustom
-  export let limitCustom
-  export let valueColumn
-  export let labelColumn
+  export let tableId;
+  export let filterCustom;
+  export let limitCustom;
+  export let valueColumn;
+  export let labelColumn;
 
-  export let pickerColumns = []
-  export let searchColumns
-  
-  export let multi
-  export let icon
+  export let pickerColumns = [];
+  export let searchColumns;
+
+  export let multi;
+  export let icon;
 
   let formField;
   let formStep;
   let fieldState;
   let fieldApi;
-  let fieldSchema
+  let fieldSchema;
   let value;
-  let cellState
-  let definition
-  let loaded
-  let innerLabelColumn = labelColumn
-  let innerValueColumn
-  let innerPickerColumns
-  
+  let cellState;
+  let definition;
+  let loaded;
+  let innerLabelColumn = labelColumn;
+  let innerValueColumn;
+  let innerPickerColumns;
+
   $: formStep = formStepContext ? $formStepContext || 1 : 1;
 
   $: formField = formApi?.registerField(
@@ -64,7 +73,7 @@
     readonly,
     validation,
     formStep
-  )
+  );
 
   $: unsubscribe = formField?.subscribe((value) => {
     fieldState = value?.fieldState;
@@ -72,26 +81,32 @@
     fieldSchema = value?.fieldSchema;
   });
 
-  $: value = fieldState?.value ? fieldState.value : []
-  $: error = fieldState?.error
-  $: linkValueType == "link" && fieldSchema.tableId ? fetchDefinition(fieldSchema?.tableId) : null
+  $: value = fieldState?.value ? fieldState.value : [];
+  $: error = fieldState?.error;
+  $: linkValueType == "link" && fieldSchema.tableId
+    ? fetchDefinition(fieldSchema?.tableId)
+    : null;
 
-  $: if ( linkValueType == "link" && definition ) {
-    innerValueColumn = "_id"
-    innerLabelColumn = definition.primaryDisplay
-    innerPickerColumns = pickerColumns?.length > 0 ? pickerColumns : [ {"name" : innerLabelColumn} ]
+  $: if (linkValueType == "link" && definition) {
+    innerValueColumn = "_id";
+    innerLabelColumn = definition.primaryDisplay;
+    innerPickerColumns =
+      pickerColumns?.length > 0 ? pickerColumns : [{ name: innerLabelColumn }];
   }
 
-  $: innerLimit = limitCustom ? limitCustom : 10
+  $: innerLimit = limitCustom ? limitCustom : 10;
   $: if (
-      $builderStore.inBuilder &&
-      linkValueType == "link" &&
-      fieldSchema?.tableId &&
-      tableId?.tableId != fieldSchema?.tableId &&
-      $componentStore.selectedComponentPath?.includes($component.id)
-    ) {
-      builderStore.actions.updateProp( "tableId", { tableId : fieldSchema?.tableId , type: "table" } )
-    }
+    $builderStore.inBuilder &&
+    linkValueType == "link" &&
+    fieldSchema?.tableId &&
+    tableId?.tableId != fieldSchema?.tableId &&
+    $componentStore.selectedComponentPath?.includes($component.id)
+  ) {
+    builderStore.actions.updateProp("tableId", {
+      tableId: fieldSchema?.tableId,
+      type: "table",
+    });
+  }
 
   $: $component.styles = {
     ...$component.styles,
@@ -104,62 +119,67 @@
         labelPos == "left" ? (labelWidth ? labelWidth : "6rem") : "auto",
     },
   };
-  
-  onDestroy(() => {
-    fieldApi?.deregister()
-    unsubscribe?.()
-  })
 
-  const fetchDefinition = async tableId => {
+  onDestroy(() => {
+    fieldApi?.deregister();
+    unsubscribe?.();
+  });
+
+  const fetchDefinition = async (tableId) => {
     try {
-      definition = await API.fetchTableDefinition(tableId)
+      definition = await API.fetchTableDefinition(tableId);
     } catch (error) {
-      definition = null
+      definition = null;
     }
 
     if (!loaded) {
-      loaded = true
+      loaded = true;
     }
-  }
+  };
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
 <Block>
-  <div
-    class="superField"
-    use:styleable={$component.styles}  
-  >
-    {#if label || error }
-      <label for="superCell" class="superlabel" class:bound={formContext}>
+  <div class="superField" use:styleable={$component.styles}>
+    {#if label}
+      <label for="superCell" class="superlabel" class:left={labelPos == "left"}>
         {label}
+        {#if fieldState.error}
+          <div class="error" class:left={labelPos == "left"}>
+            <span>{fieldState.error}</span>
+          </div>
+        {/if}
       </label>
     {/if}
-    
+
     {#key loaded}
       <div class="inline-cells">
         <CellLink
           bind:cellState
-          cellOptions={{ 
+          cellOptions={{
             placeholder,
             autocomplete,
             readonly,
             disabled,
             defaultValue,
             controlType,
-            role: "formInput", 
+            role: "formInput",
             icon: icon,
             multi,
             pickerColumns,
             searchColumns,
-            }}
+          }}
           {value}
           {fieldSchema}
           tableId={tableId?.tableId}
           valueColumn={innerValueColumn}
           labelColumn={innerLabelColumn}
-          {filter},
-          multi={linkValueType == "array" || (linkValueType == "link" && fieldSchema.relationshipType != "one-to-many" )}
+          {filter}
+          ,
+          multi={linkValueType == "array" ||
+            (linkValueType == "link" &&
+              fieldSchema.relationshipType != "one-to-many")}
           {innerLimit}
           pickerColumns={innerPickerColumns ?? pickerColumns}
           on:change={(e) => fieldApi?.setValue(e.detail)}
@@ -169,72 +189,25 @@
           <div
             class="spectrum-ActionGroup spectrum-ActionGroup--compact spectrum-ActionGroup--sizeM"
           >
-          <Provider data={ { value : fieldState.value }} >
-            {#each buttons as { text, onClick , quiet, disabled, type }}
-              <BlockComponent
-                type = "plugin/bb-component-SuperButton"
-                props = {{
-                  quiet,
-                  disabled, 
-                  size: "M",
-                  text,
-                  onClick,
-                  emphasized : true,
-                  selected: type == "cta"
-                }}>
-                </BlockComponent>
+            <Provider data={{ value: fieldState.value }}>
+              {#each buttons as { text, onClick, quiet, disabled, type }}
+                <BlockComponent
+                  type="plugin/bb-component-SuperButton"
+                  props={{
+                    quiet,
+                    disabled,
+                    size: "M",
+                    text,
+                    onClick,
+                    emphasized: true,
+                    selected: type == "cta",
+                  }}
+                ></BlockComponent>
               {/each}
-          </Provider>
-        </div>
-      {/if}
+            </Provider>
+          </div>
+        {/if}
       </div>
     {/key}
-
   </div>
 </Block>
-
-<style>
-  .superField {
-    display: flex;
-    align-items: stretch;
-    justify-content: stretch;
-    min-width: 0;
-  }
-
-  .superField:focus {
-    outline: none;
-  }
-  .superlabel {
-    display: flex;
-    align-items: flex-start;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-    min-width: var(--label-width);
-    max-width: var(--label-width);
-    font-size: 12px;
-    line-height: 1.75rem;
-    font-weight: 400;
-    color: var(--spectrum-global-color-gray-700);
-  }
-
-  .inline-cells {
-    flex: 1;
-    display: flex;
-    justify-items: stretch;
-    height: 2rem;
-  }
-
-  .disabled {
-    color: var(--spectrum-global-color-gray-600);
-    background-color: var(--spectrum-global-color-gray-200);
-  }
-
-  .superlabel.bound {
-    gap: 0.5rem;
-  }
-</style>
-
-
-
-
